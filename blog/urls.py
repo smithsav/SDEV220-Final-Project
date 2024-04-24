04c425c98a91d8feabc07884eff72505a11b5ff5
smithsav <ssmith1695@ivytech.edu>
2024-04-23 20:51:51 -0500

    urls added

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='home.html'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('view_inventory/', views.view_inventory, name='view_inventory'),
+    path('record_sale/', views.record_sale, name='record_sale'),
 ]
