69583ed02b55b65bce2bddf30851fc6d83883c28
smithsav <ssmith1695@ivytech.edu>
2024-04-24 17:44:15 -0500

    html update & views update for add_product

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
+from .models import Products  
+from .forms import AddProductForm
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
 def add_product(request):
+    if request.method == 'POST':
+        form = AddProductForm(request.POST)
+        if form.is_valid():
+            product_name = form.cleaned_data['product_name']
+            product_quantity = form.cleaned_data['product_quantity']
+            product_price = form.cleaned_data['product_price']
+            
+            products_manager = Products()
+            
+            products_manager.add_product(product_name, product_quantity, product_price)
+            
+            return render(request, 'add_products.html', {'form': form, 'product': {'name': product_name, 'quantity': product_quantity, 'price': product_price}})
+    else:
+        form = AddProductForm()
+    
     return render(request, 'blog/add_product.html', {})
 
 def view_inventory(request):
