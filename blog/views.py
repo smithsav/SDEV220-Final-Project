0705bb9c8cb6cba45982214599cd18b213c0237f
smithsav <ssmith1695@ivytech.edu>
2024-04-23 20:54:56 -0500

    ..

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def add_product(request):
+    return render(request, 'blog/add_product.html', {})
+
+def view_inventory(request):
+    return render(request, 'blog/view_inventory.html', {})
+
+def record_sale(request):
+    return render(request, 'blog/record_sale.html', {})
