343c28a43dde983096d2012329b901ca96d33a4d
smithsav <ssmith1695@ivytech.edu>
2024-04-24 18:04:09 -0500

    ..

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='home.html'),
-    path('add_product/', views.add_product, name='add_product'),
-    path('view_inventory/', views.view_inventory, name='view_inventory'),
-    path('record_sale/', views.record_sale, name='record_sale'),
-    path('add_customerID/', views.add_customerID, name='add_customerID'),
+    path('add_product/', views.add_product, name='add_products.html'),
+    path('view_inventory/', views.view_inventory, name='view_inventory.htmlh'),
+    path('record_sale/', views.record_sale, name='record_sale.html'),
+    path('add_customerID/', views.add_customerID, name='add_customerID.html'),
 ]
 
 if settings.DEBUG:
