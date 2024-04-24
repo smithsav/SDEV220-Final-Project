03cab7bd939c7549e81447d57217f955f90a94f3
smithsav <ssmith1695@ivytech.edu>
2024-04-24 18:10:45 -0500

    ..

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.post_list, name='home.html'),
-    path('add_product/', views.add_product, name='add_products.html'),
+    path('', views.post_list, name='base.html'),
+    path('add_product/', views.add_product, name='add_product.html'),
     path('view_inventory/', views.view_inventory, name='view_inventory.htmlh'),
     path('record_sale/', views.record_sale, name='record_sale.html'),
     path('add_customerID/', views.add_customerID, name='add_customerID.html'),
