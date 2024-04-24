86a947717b7d6413174572053a53060b88a9d25d
smithsav <ssmith1695@ivytech.edu>
2024-04-24 16:47:02 -0500

    ..

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('', views.post_list, name='home.html'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('view_inventory/', views.view_inventory, name='view_inventory'),
+    path('record_sale/', views.record_sale, name='record_sale'),
+    path('add_customerID/', views.add_customerID, name='add_customerID'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
