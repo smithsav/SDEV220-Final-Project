7def1a095c9187d02c9f3903fca1c2cbdfdc6b10
smithsav <ssmith1695@ivytech.edu>
2024-04-29 18:59:46 -0500

    from added

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
