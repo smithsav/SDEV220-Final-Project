5bd3c8032ec2f7ebb9bc64592970a36f070c201d
smithsav <ssmith1695@ivytech.edu>
2024-04-30 19:04:24 -0500

    forms

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .products import Products  
 from .forms import AddProductForm
 from .models import Product
 from .inventory import Inventory 
+from .forms import ProductForm
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
 def add_product(request):
     if request.method == 'POST':
-        form = AddProductForm(request.POST)
+        form = ProductForm(request.POST)
         if form.is_valid():
-            form.save()
+            form.save()  
             return redirect('view_inventory')  
     else:
-        form = AddProductForm()
+        form = ProductForm()
     
-    products = Product.objects.all()  
-    return render(request, 'add_products.html', {'form': form, 'products': products})
+    return render(request, 'add_product.html', {'form': form})
 
 def view_inventory(request):
-    return render(request, 'view_inventory.html')
+    products = Product.objects.all()  
+    return render(request, 'view_inventory.html', {'products': products})
 
 def record_sale(request):
     return render(request, 'blog/record_sale.html', {})
