78b80f3760a88768454412c85c15d87c389b29ad
Kizzie Trinh <143278756+knttrinh@users.noreply.github.com>
2024-05-06 21:57:10 -0400

    models.py
    I have added my portion to it

--- blog/customer.py
+++ blog/customer.py
@@ -5,25 +5,21 @@ class customer():
         self.customerLName = None
         self.customerAddress = None
         self.customerNumber = None
-        self.customerpayment = None
-        self.customerID = None
-    
+          
     def customer_details(self, customerFName, customerLName, customerAddress, customerNumber, customerpayment, customerID):
         self.customerFName = customerFName
         self.customerLName = customerLName
         self.customerAddress = customerAddress
         self.customerNumber = customerNumber
-        self.customerpayment = customerpayment
-        self.customerID = customerID
-        
+                
     def remove_customer(self, customerID):
-        self.removecusmterID = customerID
+        self.removecusmter = customer
     
     def main(self):
         customerFName = input("First Name:", customerFName)
         customerLName = input("Last Name: ", customerLName)
         customerAddress = input ("Address(City, State, Zip code): ", customerAddress)
         customerNumber = int(input("Phone Number(xxx-xxx-xxxx): ", customerNumber))
-        customerpayment = ""
+      
 
     main()  
