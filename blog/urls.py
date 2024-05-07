1ae4bd5200b4b5ca870664ebf56b5491522e5d86
smithsav <ssmith1695@ivytech.edu>
2024-05-06 20:01:14 -0500

    ..

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('record_sale/', views.record_sale, name='record_sale'),
     path('add_customerID/', views.add_customerID, name='add_customerID'),
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
