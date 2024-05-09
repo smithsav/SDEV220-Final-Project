84971407df435b53b635e1c2997687ab29d29d59
smithsav <ssmith1695@ivytech.edu>
2024-05-09 18:15:10 -0500

    blog_product

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='product',
+            name='blog_product',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
