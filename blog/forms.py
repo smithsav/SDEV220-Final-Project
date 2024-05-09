c9455785839b8dc04aa552db86baf57fcaf46a59
Kizzie Trinh <143278756+knttrinh@users.noreply.github.com>
2024-05-08 22:23:57 -0400

    Add files via upload

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import product
 from django.shortcuts import render , redirect
-import calculation
+
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -18,13 +19,9 @@ class customer(forms.Form):
     customerAddress = forms.CharField()
     customerNumber = forms.IntegerField()
 
-class totalsales(forms.Form):
+class record_sale(forms.Form):
     quantity = forms.DecimalField()
     price = forms.DecimalField()
-    amount = forms.DecimalField(
-        widget=calculation.FormulaInput('quantity*price')
-    )
+    amount = forms.DecimalField()
     apply_taxes = forms.BooleanField(initial=True)
-    tax = forms.DecimalField(
-        widget=calculation.FormulaInput('apply_taxes ? parseFloat(amount/11).toFixed(2) : 0.0') 
-    )
+    tax = forms.DecimalField()
