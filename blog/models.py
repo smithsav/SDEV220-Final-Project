ab33b4d42754430c04906fb7170e5e6723357b79
smithsav <ssmith1695@ivytech.edu>
2024-05-12 07:19:19 -0500

    revert

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.db.models.signals import pre_save
-import sqlite3
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -29,41 +28,15 @@ class product(models.Model):
     class Meta:
         db_table = 'product'
 
-# Define a function to populate the database from the text file
-def populate_database_from_file(file_path):
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-    with open(file_path, 'r') as file:
-        for line in file:
-            first_name, last_name = line.strip().split()
-            address = "Address"  # Placeholder for now
-            phone_number = "Phone Number"  # Placeholder for now
-            # Insert the customer into the database
-            cursor.execute("INSERT INTO customer (first_name, last_name, address, phone_number) VALUES (?, ?, ?, ?)",
-                           (first_name, last_name, address, phone_number))
-    conn.commit()
-    conn.close()
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, default='')
 
-# Define a function to search for customers in the database
-def search_customers_in_database(search_query):
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM customer WHERE first_name LIKE ? OR last_name LIKE ?", ('%' + search_query + '%', '%' + search_query + '%'))
-    found_customers = cursor.fetchall()
-    conn.close()
-    return found_customers
-
-# Example usage
-file_path = 'blog/customername.txt' 
-search_query = input("Enter search query: ")
-
-# Populate the database (only do this once, or whenever the text file is updated)
-populate_database_from_file(file_path)
-
-# Search for customers in the database
-found_customers = search_customers_in_database(search_query)
-for customer in found_customers:
-    print(f"{customer[1]} {customer[2]} - {customer[3]} - {customer[4]}")
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    class Meta:
+        db_table = 'customer'
 
 class RecordSale(models.Model):
     product_quantity = models.IntegerField()
